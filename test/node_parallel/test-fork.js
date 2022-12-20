5fc3af19adfa5ed0b9a4e07d33cbaf3d0d8c73eb
Chengzhong Wu <chengzhong.wcz@alibaba-inc.com>
2022-12-20 11:45:11 +0800

    fix: stop loop once the process is ready for fork (#13)

--- test/node_parallel/test-fork.js
+++ test/node_parallel/test-fork.js
@@ -32,3 +32,12 @@ promise_test(async () => {
   assert_regexp_match(result.stderr.trim(), /Not run in a warm-forkable environment. You may be running in a testing environment./);
   assert_regexp_match(result.stdout.trim(), /main,serialize,deserialize,expected-rejection,install,installed/);
 }, 'fork with userland unfinished fetch');
+
+promise_test(async () => {
+  const result = await common.execAworker([
+    '--mode=seed-userland',
+    path.join(__dirname, '../fixtures/fork/fork-userland-dangling-timer.js'),
+  ]);
+  assert_regexp_match(result.stderr.trim(), /Not run in a warm-forkable environment. You may be running in a testing environment./);
+  assert_regexp_match(result.stdout.trim(), /^main,serialize,deserialize,install$/);
+}, 'fork with dangling timer');
