998f8d35f315fa571f3b177c19982f7a08b1cb27
mariodu <dujiakun@gmail.com>
2023-03-08 09:48:56 +0000

    fix: time origin dynamic in snapshot

---
+++ test/node_parallel/test-performance-time-origin.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const path = require('path');
+const common = require('../common/node_testharness');
+
+promise_test(async () => {
+  const instance_1 = await common.execAworker([ path.join(__dirname, 'fixtures', 'performance-time-origin.js') ]).catch(e => e);
+  const instance_2 = await common.execAworker([ path.join(__dirname, 'fixtures', 'performance-time-origin.js') ]).catch(e => e);
+
+  assert_true(!isNaN(parseFloat(instance_1.stdout)));
+  assert_true(!isNaN(parseFloat(instance_2.stdout)));
+
+  assert_true(instance_1.stdout != instance_2.stdout);
+}, 'performance.timeOrigin between aworker instance');
