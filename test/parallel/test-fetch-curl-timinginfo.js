7e376b685f329835022dd7a086b0962fa2a1e1f3
liyilong.lyl <liyilong.lyl@alibaba-inc.com>
2023-04-10 19:34:51 +0800

    f

--- test/parallel/test-fetch-curl-timinginfo.js
+++ test/parallel/test-fetch-curl-timinginfo.js
@@ -4,7 +4,7 @@
 promise_test(async function() {
   const res = await fetch('http://localhost:30122/echo', { method: 'GET' });
   const timingInfo = res[Symbol.for('aworker:fetch_timing_info')];
-  assert_true(timingInfo instanceof Object);
+  assert_equals(typeof timingInfo, 'object');
   assert_equals(typeof timingInfo.totalTime, 'number');
   assert_equals(typeof timingInfo.namelookupTime, 'number');
   assert_equals(typeof timingInfo.connectTime, 'number');
@@ -13,3 +13,18 @@ promise_test(async function() {
   assert_equals(typeof timingInfo.starttransferTime, 'number');
   assert_equals(typeof timingInfo.redirectTime, 'number');
 }, 'timinginfo property');
+
+promise_test(async function() {
+  const res = await fetch('http://localhost:30122/echo', { method: 'GET' });
+  const timingInfo = res[Symbol.for('aworker:fetch_timing_info')];
+  assert_true(timingInfo.totalTime > timingInfo.starttransferTime);
+  assert_true(timingInfo.starttransferTime > timingInfo.pretransferTime);
+  // only valid for SSL connect
+  assert_equals(timingInfo.appconnectTime, 0);
+  assert_true(timingInfo.pretransferTime > timingInfo.connectTime);
+  assert_true(timingInfo.connectTime >= timingInfo.namelookupTime);
+  assert_true(timingInfo.namelookupTime > 0);
+
+  // not implement
+  assert_true(timingInfo.redirectTime >= 0);
+}, 'property value');
