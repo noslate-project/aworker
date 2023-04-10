4afb20124a48c69164f17614a044e4185cb24aaf
liyilong.lyl <liyilong.lyl@alibaba-inc.com>
2023-04-10 18:58:52 +0800

    f

--- test/parallel/test-fetch-curl-timinginfo.js
+++ test/parallel/test-fetch-curl-timinginfo.js
@@ -3,7 +3,7 @@
 
 promise_test(async function() {
   const res = await fetch('http://localhost:30122/echo', { method: 'GET' });
-  const timingInfo = res[Symbol.for('aworker:fetch_timing_info')]
+  const timingInfo = res[Symbol.for('aworker:fetch_timing_info')];
   assert_true(timingInfo instanceof Object);
   assert_equals(typeof timingInfo.totalTime, 'number');
   assert_equals(typeof timingInfo.namelookupTime, 'number');
