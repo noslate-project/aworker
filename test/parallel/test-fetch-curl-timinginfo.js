4906aba314ef44a7a33d5ea07440f70038f28b94
liyilong.lyl <liyilong.lyl@alibaba-inc.com>
2023-04-10 18:55:10 +0800

    f

---
+++ test/parallel/test-fetch-curl-timinginfo.js
@@ -0,0 +1,15 @@
+// META: flags=--experimental-curl-fetch
+'use strict';
+
+promise_test(async function() {
+  const res = await fetch('http://localhost:30122/echo', { method: 'GET' });
+  const timingInfo = res[Symbol.for('aworker:fetch_timing_info')]
+  assert_true(timingInfo instanceof Object);
+  assert_equals(typeof timingInfo.totalTime, 'number');
+  assert_equals(typeof timingInfo.namelookupTime, 'number');
+  assert_equals(typeof timingInfo.connectTime, 'number');
+  assert_equals(typeof timingInfo.appconnectTime, 'number');
+  assert_equals(typeof timingInfo.pretransferTime, 'number');
+  assert_equals(typeof timingInfo.starttransferTime, 'number');
+  assert_equals(typeof timingInfo.redirectTime, 'number');
+}, 'timinginfo property');
