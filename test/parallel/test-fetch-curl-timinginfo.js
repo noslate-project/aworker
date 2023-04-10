5a9960d97743819c5447a6410a165975cd2cb4fb
liyilong.lyl <liyilong.lyl@alibaba-inc.com>
2023-04-10 20:00:41 +0800

    g

--- test/parallel/test-fetch-curl-timinginfo.js
+++ test/parallel/test-fetch-curl-timinginfo.js
@@ -5,6 +5,7 @@ promise_test(async function() {
   const res = await fetch('http://localhost:30122/echo', { method: 'GET' });
   const timingInfo = res[Symbol.for('aworker:fetch_timing_info')];
   assert_equals(typeof timingInfo, 'object');
+  assert_equals(typeof timingInfo.startTime, 'number');
   assert_equals(typeof timingInfo.totalTime, 'number');
   assert_equals(typeof timingInfo.namelookupTime, 'number');
   assert_equals(typeof timingInfo.connectTime, 'number');
@@ -15,8 +16,11 @@ promise_test(async function() {
 }, 'timinginfo property');
 
 promise_test(async function() {
+  const now = Date.now();
   const res = await fetch('http://localhost:30122/echo', { method: 'GET' });
   const timingInfo = res[Symbol.for('aworker:fetch_timing_info')];
+  assert_true(timingInfo.startTime >= now);
+  assert_true(Date.now() > timingInfo.startTime);
   assert_true(timingInfo.totalTime > timingInfo.starttransferTime);
   assert_true(timingInfo.starttransferTime > timingInfo.pretransferTime);
   // only valid for SSL connect
