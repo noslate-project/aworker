a7f95749d1397abdd162913fb887f6c63bdf22ae
Chengzhong Wu <chengzhong.wcz@alibaba-inc.com>
2023-05-18 14:16:47 +0800

    fix: foreground task runner cross thread task posting (#39)
    Refs: AONE#49636368

--- src/aworker_platform.cc
+++ src/aworker_platform.cc
@@ -81,92 +81,117 @@ class AworkerTraceStateObserver
   v8::TracingController* controller_;
 };
 
-void SameThreadTaskRunner::ImmediateProcessor(uv_timer_t* handle) {
-  SameThreadTaskRunner* runner =
-      static_cast<SameThreadTaskRunner*>(handle->data);
+// static
+std::shared_ptr<ForegroundTaskRunner> ForegroundTaskRunner::New(
+    uv_loop_t* loop) {
+  return std::shared_ptr<ForegroundTaskRunner>(new ForegroundTaskRunner(loop),
+                                               Delete);
+}
 
-  runner->DrainTasks();
+// static
+void ForegroundTaskRunner::TimerProcessor(uv_timer_t* timer) {
+  ForegroundTaskRunner* runner =
+      ContainerOf(&ForegroundTaskRunner::timer_, timer);
+  runner->RunTasks();
+}
+
+// static
+void ForegroundTaskRunner::ImmediateProcessor(uv_async_t* async) {
+  ForegroundTaskRunner* runner =
+      ContainerOf(&ForegroundTaskRunner::async_, async);
+  runner->RunTasks();
+}
+
+void ForegroundTaskRunner::RunTasks() {
+  DrainTasks();
 
   uint64_t now = now_in_ms();
   uint64_t next_timeout = 0;
-  if (runner->tasks_.size() > 0) {
-    next_timeout = 1;
-  } else if (runner->delayed_tasks_.size() > 0) {
-    next_timeout =
-        std::max(runner->delayed_tasks_.top()->deadline() - now, uint64_t(1));
+  {
+    std::scoped_lock guard(queue_mutex_);
+    if (delayed_tasks_.size() > 0) {
+      next_timeout =
+          std::max(delayed_tasks_.top()->deadline() - now, uint64_t(1));
+    }
   }
   if (next_timeout == 0) {
-    runner->next_deadline_ = -1;
+    uv_timer_stop(&timer_);
     return;
   }
   per_process::Debug(
       DebugCategory::PLATFORM, "scheduling next immediate: %d\n", next_timeout);
-  runner->next_deadline_ = now + next_timeout;
-  uv_timer_start(runner->timer_, ImmediateProcessor, next_timeout, 0);
+  uv_timer_start(&timer_, TimerProcessor, next_timeout, 0);
 }
 
-void SameThreadTaskRunner::DrainTasks() {
+void ForegroundTaskRunner::DrainTasks() {
   uint64_t now = now_in_ms();
   std::list<std::unique_ptr<PlatformTask>> tasks;
-  tasks_.swap(tasks);
-  for (auto it = tasks.begin(); it != tasks.end(); it++) {
-    (*it)->Run();
-  }
-
-  while (delayed_tasks_.size() > 0) {
-    const unique_ptr<PlatformDelayedTask>& it = delayed_tasks_.top();
-    if (it->deadline() > now) {
-      break;
+  std::list<std::unique_ptr<PlatformDelayedTask>> delayed_tasks;
+  {
+    std::scoped_lock guard(queue_mutex_);
+    tasks_.swap(tasks);
+    while (delayed_tasks_.size() > 0) {
+      auto& it = const_cast<std::unique_ptr<PlatformDelayedTask>&>(
+          delayed_tasks_.top());
+      if (it->deadline() > now) {
+        break;
+      }
+      delayed_tasks.push_back(std::move(it));
+      delayed_tasks_.pop();
     }
+  }
+  for (auto& it : tasks) {
     it->Run();
-    delayed_tasks_.pop();
+  }
+  for (auto& it : delayed_tasks) {
+    it->Run();
   }
 }
 
-SameThreadTaskRunner::SameThreadTaskRunner(uv_loop_t* loop)
-    : loop_(loop), timer_(new uv_timer_t()), next_deadline_(0) {
-  uv_timer_init(loop, timer_);
-  uv_unref(reinterpret_cast<uv_handle_t*>(timer_));
-  timer_->data = this;
-  // Set to UINT64_MAX;
-  next_deadline_--;
+ForegroundTaskRunner::ForegroundTaskRunner(uv_loop_t* loop) {
+  uv_timer_init(loop, &timer_);
+  uv_async_init(loop, &async_, ImmediateProcessor);
+  uv_unref(reinterpret_cast<uv_handle_t*>(&timer_));
+  uv_unref(reinterpret_cast<uv_handle_t*>(&async_));
 }
 
-SameThreadTaskRunner::~SameThreadTaskRunner() {
-  uv_close(reinterpret_cast<uv_handle_t*>(timer_), [](uv_handle_t* handle) {
-    uv_timer_t* timer = reinterpret_cast<uv_timer_t*>(handle);
-    delete timer;
-  });
+ForegroundTaskRunner::~ForegroundTaskRunner() {}
+
+void ForegroundTaskRunner::Delete(ForegroundTaskRunner* runner) {
+  uv_close(reinterpret_cast<uv_handle_t*>(&runner->timer_), nullptr);
+  uv_close(reinterpret_cast<uv_handle_t*>(&runner->async_),
+           [](uv_handle_t* handle) {
+             ForegroundTaskRunner* runner =
+                 ContainerOf(&ForegroundTaskRunner::async_,
+                             reinterpret_cast<uv_async_t*>(handle));
+             delete runner;
+           });
 }
 
-void SameThreadTaskRunner::PostTask(unique_ptr<v8::Task> task) {
-  uint64_t deadline = now_in_ms();
-  tasks_.push_back(std::make_unique<PlatformTask>(move(task)));
-
-  if (deadline < next_deadline_) {
-    uv_timer_start(timer_, ImmediateProcessor, 0, 0);
-    next_deadline_ = deadline;
+void ForegroundTaskRunner::PostTask(unique_ptr<v8::Task> task) {
+  {
+    std::scoped_lock guard(queue_mutex_);
+    tasks_.push_back(std::make_unique<PlatformTask>(move(task)));
   }
+  uv_async_send(&async_);
 }
 
-void SameThreadTaskRunner::PostDelayedTask(unique_ptr<v8::Task> task,
+void ForegroundTaskRunner::PostDelayedTask(unique_ptr<v8::Task> task,
                                            double delay) {
-  uint64_t now = now_in_ms();
-  uint64_t deadline = now + std::llround(delay * 1000);
-  delayed_tasks_.push(
-      std::make_unique<PlatformDelayedTask>(move(task), deadline));
-
-  if (deadline < next_deadline_) {
-    uv_timer_start(timer_, ImmediateProcessor, deadline - now, 0);
-    next_deadline_ = deadline;
+  uint64_t deadline = now_in_ms() + std::llround(delay * 1000);
+  {
+    std::scoped_lock guard(queue_mutex_);
+    delayed_tasks_.push(
+        std::make_unique<PlatformDelayedTask>(move(task), deadline));
   }
+  uv_async_send(&async_);
 }
 
-void SameThreadTaskRunner::PostNonNestableTask(unique_ptr<v8::Task> task) {
+void ForegroundTaskRunner::PostNonNestableTask(unique_ptr<v8::Task> task) {
   PostTask(move(task));
 }
 
-void SameThreadTaskRunner::PostNonNestableDelayedTask(unique_ptr<v8::Task> task,
+void ForegroundTaskRunner::PostNonNestableDelayedTask(unique_ptr<v8::Task> task,
                                                       double delay) {
   PostDelayedTask(move(task), delay);
 }
@@ -175,7 +200,7 @@ AworkerPlatform::AworkerPlatform(ThreadMode thread_mode, int thread_pool_size)
     : thread_mode_(thread_mode) {
   CHECK_EQ(uv_loop_init(&loop_), 0);
   CHECK_EQ(uv_loop_configure(&loop_, UV_METRICS_IDLE_TIME), 0);
-  task_runner_ = std::make_unique<aworker::SameThreadTaskRunner>(&loop_);
+  task_runner_ = ForegroundTaskRunner::New(&loop_);
   trace_agent_ = std::make_unique<TraceAgent>(&loop_);
   aworker::TracingController* controller = trace_agent_->GetTracingController();
   trace_state_observer_ =
--- test/parallel/test-fetch-curl-timinginfo.js
+++ test/parallel/test-fetch-curl-timinginfo.js
@@ -29,7 +29,7 @@ promise_test(async function() {
   const res = await fetch('http://localhost:30122/echo', { method: 'GET' });
   const timingInfo = res[Symbol.for('aworker:fetch_timing_info')];
   assert_true(timingInfo.startTime >= now);
-  assert_true(Date.now() > timingInfo.startTime);
+  assert_true(Date.now() >= timingInfo.startTime);
   assert_true(timingInfo.totalTime > timingInfo.starttransferTime);
   assert_true(timingInfo.starttransferTime > timingInfo.pretransferTime);
   // only valid for SSL connect
